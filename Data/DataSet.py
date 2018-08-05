ddaee9b7b5428e4a0ad3717d32062635576983d3
Madonahs <syombuamadona@gmail.com>
2018-08-04 23:20:35 -0500

    playing_with_data

--- Data/DataSet.py
+++ Data/DataSet.py
@@ -4,15 +4,15 @@ Created on Sat Aug  4 22:32:36 2018
 @author: madon
 """
 
-nestedList = [["California",25,120],
-              ["California",18,30],
-              ["California",22,50],
-              ["Birmingham",43,60],
-              ["California",50,100],
+nestedList = [["EastLeigh",25,120],
+              ["Buru Buru",18,30],
+              ["Lavington",22,50],
+              ["Kileleshwa",43,60],
+              ["Kitusuru",50,100],
              ]
 
 print("Location : Age : Monthly Payments ")
 
 for item in nestedList:
-    print(item[0]," : " * (10-len(item[0])),":",
-          item[1]," " *(14-len(str(item[1]))),item[2]," " *(4-len(str(item[2]))),)
+    print(item[0]," : " * (7-len(item[0])),":",
+          item[1]," " *(9-len(str(item[1]))),item[2]," " *(4-len(str(item[2]))),)
