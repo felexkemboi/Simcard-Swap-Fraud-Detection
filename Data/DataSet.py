4034b82fb9608a1b3841e4ea21bb08184f830c28
Madonahs <syombuamadona@gmail.com>
2018-08-04 22:57:05 -0500

    draft_data

--- Data/DataSet.py
+++ Data/DataSet.py
@@ -4,11 +4,11 @@ Created on Sat Aug  4 22:32:36 2018
 @author: madon
 """
 
-nestedList = [["Birmingham",30,60],
-              ["Birmingham",30,60],
-              ["Birmingham",30,60],
-              ["Birmingham",30,60],
-              ["Birmingham",30,60],
+nestedList = [["NewYork",25,120],
+              ["Boston",18,30],
+              ["Manhattan",22,50],
+              ["Birmingham",43,60],
+              ["California",50,100],
              ]
 
 print("Location : Age : Monthly Payments ")
